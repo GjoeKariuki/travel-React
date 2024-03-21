2069aaa9315c21a58a4507f974d6fbc17e5a8f25
GjoeKariuki <githaigageorge96@gmail.com>
2024-03-21 16:57:28 +0300

    root styles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import './navbar.css'
 import {BiLogoMediumOld} from 'react-icons/bi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {PiDotsNineBold} from 'react-icons/pi'
 
 
 function Navbar() {
   return (
+  <div className='navBar'>
     <div className='logoDiv'>
       <BiLogoMediumOld className="icon" />
-      <span></span>
-      </div>
+      <span>Twende Safaris</span>
+    </div>
+    <div className='menu'>
+      <ul>
+        <li className='navList'>
+          Destination
+        </li>
+        <li className='navList'>
+          About Us
+        </li>
+        <li className='navList'>
+          Testimonials
+        </li>
+        <li className='navList'>
+          Gallery
+        </li>
+      </ul>
+      {/* icon to close navbar */}
+      <AiFillCloseCircle className='icon' />
+    </div>
+    <button className="signUpBtn btn">
+        Sign Up
+    </button>
+    {/* icon to toggle navBar */}
+    <PiDotsNineBold className='icon' />
+  </div>
+    
   )
 }
 
